92f97685766f3f70ac36e0c9abc45ea45caedc98
Njan-b <dhrishyadasph2005@gmail.com>
2026-02-12 17:19:07 +0530

    initial commit

--- src/ComplexAddition.java
+++ src/ComplexAddition.java
@@ -1,23 +1,40 @@
 import java.util.Scanner;
 
-class Complex {
-    
-
-    // TODO: parameterized constructor Complex(int real, int imag)
-
-    // TODO: Method to add two complex numbers: add(Complex other) returning new Complex
-    // Or static method add(Complex c1, Complex c2)
-}
-
 public class ComplexAddition {
-    
-        // TODO: Read real1, imag1
-        // TODO: Read real2, imag2
-        
-        // TODO: Create two Complex objects
-        
-        // TODO: Add them
-        
-        // TODO: Print result in "real + imagi" format (e.g., "6 + 8i")
-    
+
+    int real;
+    int imaginary;
+
+    // Constructor
+    ComplexAddition(int real, int imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    // Method to add two complex numbers
+    ComplexAddition add(ComplexAddition c) {
+        int sumReal = this.real + c.real;
+        int sumImaginary = this.imaginary + c.imaginary;
+        return new ComplexAddition(sumReal, sumImaginary);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int r1 = sc.nextInt();
+        int i1 = sc.nextInt();
+
+        int r2 = sc.nextInt();
+        int i2 = sc.nextInt();
+
+        ComplexAddition c1 = new ComplexAddition(r1, i1);
+        ComplexAddition c2 = new ComplexAddition(r2, i2);
+
+        ComplexAddition result = c1.add(c2);
+
+        System.out.println(result.real + " + " + result.imaginary + "i");
+
+        sc.close();
+    }
 }
